a75046c5222476f53e0db57e985dbce6e84f5243
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-07-30 15:51:14 -0700

    yo yo pretty

--- shaders/fs-tree.glsl
+++ shaders/fs-tree.glsl
@@ -8,6 +8,8 @@ uniform vec3 lightPositions[5];
 uniform sampler2D lightTextures[5];
 uniform vec3 cameraPos;
 
+uniform float normalScale;
+uniform float texScale;
 uniform float lightCutoff;
 uniform float lightPower;
 
@@ -23,7 +25,42 @@ const float timeMatMultiplier = .1;
 
 void main(){
 
-  vec3 fNorm = vNorm;
+
+  
+// FROM @thespite
+  vec3 n = normalize( vNorm.xyz );
+  vec3 blend_weights = abs( n );
+  blend_weights = ( blend_weights - 0.2 ) * 7.;  
+  blend_weights = max( blend_weights, 0. );
+  blend_weights /= ( blend_weights.x + blend_weights.y + blend_weights.z );
+
+  vec2 coord1 = vPos.yz * texScale;
+  vec2 coord2 = vPos.zx * texScale;
+  vec2 coord3 = vPos.xy * texScale;
+
+  vec3 bump1 = texture2D( t_normal , coord1 + vec2( timer * .1 , timer * .2 ) * timeMatMultiplier    ).rgb;  
+  vec3 bump2 = texture2D( t_normal , coord2 + vec2( timer * .13 , timer * .083 ) * timeMatMultiplier   ).rgb;  
+  vec3 bump3 = texture2D( t_normal , coord3 + vec2( timer * .05 , timer * .15 ) * timeMatMultiplier   ).rgb; 
+
+  vec3 blended_bump = bump1 * blend_weights.xxx +  
+                      bump2 * blend_weights.yyy +  
+                      bump3 * blend_weights.zzz;
+
+  vec3 tanX = vec3( vNorm.x, -vNorm.z, vNorm.y);
+  vec3 tanY = vec3( vNorm.z, vNorm.y, -vNorm.x);
+  vec3 tanZ = vec3(-vNorm.y, vNorm.x, vNorm.z);
+  vec3 blended_tangent = tanX * blend_weights.xxx +  
+                         tanY * blend_weights.yyy +  
+                         tanZ * blend_weights.zzz; 
+
+  vec3 normalTex = blended_bump * 2.0 - 1.0;
+  normalTex.xy *= normalScale;
+  normalTex.y *= -1.;
+  normalTex = normalize( normalTex );
+  mat3 tsb = mat3( normalize( blended_tangent ), normalize( cross( vNorm, blended_tangent ) ), normalize( vNorm ) );
+ 
+ // vec3 bump = texture2D( tNormal , vUv ).xyz;
+  vec3 fNorm = tsb * normalTex;
  
   vec3 camDir   = normalize( vMPos - cameraPos);
 
